f07c4d29d3563ec0a85b4dd4415cb6fd2fe8344a
chintala sanjay <61927341+chsanjay920@users.noreply.github.com>
2024-03-02 08:21:13 +0530

    Worked on Authentication and login screen

--- FrontEnd/src/app/app.component.ts
+++ FrontEnd/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AuthenticationService } from './Services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
-  isLogedIn = false;
-  isAdmin = true;
-  AccountName = "sanjay"
+  title = 'Frontend';
+  isLogedIn: boolean = false;
+  isAdmin: boolean = false;
+  AccountName = '';
+  private subscription: any;
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router
+  ) {}
+  ngOnInit(): void {
+    this.subscription = this.authService.IsLogedIn.subscribe((data) => {
+      this.isLogedIn = data.IsLogin;
+      this.isAdmin = data.IsAdmin;
+      this.AccountName = this.authService.getUserName();
+    });
 
-  logout()
-  {
-    
+    this.authService.checkToken();
+    this.AccountName = this.authService.getUserName();
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+  logout() {
+    this.authService.deleteToken();
+    this.router.navigateByUrl('/');
   }
 }
